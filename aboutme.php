63f9cf781d842f7cc155929beb16333876af3f4e
Hadi Firmansyah <61578530+Hadi-Firmansyah@users.noreply.github.com>
2020-04-08 12:36:23 +0700

    first
    Upload file

---
+++ aboutme.php
@@ -0,0 +1,6 @@
+<?php
+echo ' Nama : Hadi Firmansyah <br>';
+echo ' Kelas : XI RPL 2 <br>';
+echo ' Asal Sekolah : SMK Taruna Bhakti <br>';
+echo ' Alamat Rumah : Jl.Sumurbandung 3 RT 04/02 <br>';
+?>
