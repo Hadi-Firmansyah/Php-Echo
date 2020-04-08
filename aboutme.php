53920a6f32d1c82cb6b8c08ca90ae166eec241b4
Hadi Firmansyah <61578530+Hadi-Firmansyah@users.noreply.github.com>
2020-04-08 12:41:31 +0700

    simpan kembali
    Add HP & Email And Delete Address

--- aboutme.php
+++ aboutme.php
@@ -2,5 +2,6 @@
 echo ' Nama : Hadi Firmansyah <br>';
 echo ' Kelas : XI RPL 2 <br>';
 echo ' Asal Sekolah : SMK Taruna Bhakti <br>';
-echo ' Alamat Rumah : Jl.Sumurbandung 3 RT 04/02 <br>';
+echo ' No Hp : 085771364038 <br>';
+echo ' Email : hadifirmansyah.id@gmail.com <br>';
 ?>
